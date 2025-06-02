2a72f2508769ff90b654ed2cfb72b8671f9c5e5f
alenorgue <alenoriegag@gmail.com>
2025-06-02 19:08:06 +0200

    Implement API endpoint to retrieve random words with optional length filter

--- index.js
+++ index.js
@@ -18,7 +18,21 @@ app.use(express.static(path.join(__dirname, "public")));
 // Iteración 1
 
 // Iteración 2
+app.get('/api/v1/words', (req, res) => {
+  const length = parseInt(req.query.length);
+  const word = lodash.sample(words);
 
+  if (length) {
+    const filteredWords = words.filter(word => word.length === length);
+    const word = lodash.sample(filteredWords);
+    if (filteredWords.length === 0) {
+      return res.status(404).json({ error: 'No hay palabras con esa longitud' });
+    }
+    return res.json(word);
+  }
+
+  res.json(word);
+});
 // Iteración 3
 
 // Iteración 4
